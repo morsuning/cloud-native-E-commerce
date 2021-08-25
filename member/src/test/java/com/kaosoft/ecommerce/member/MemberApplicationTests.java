7c621e65766da620862f2f92ca82518b693dcd53
morsuning <morsuning@gmail.com>
2021-08-25 08:46:42 +0800

    Init

---
+++ member/src/test/java/com/kaosoft/ecommerce/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaosoft.ecommerce.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
